d6999de359eedb49b6bbea442b483dc900d89cd1
Boris <borisleighton@gmail.com>
2020-08-17 15:53:40 -0400

    Modificacion de alert en JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,11 @@ $(function() {
     $("a").click(function(event) {
     if (this.hash!=="") {
         event.preventDefault();
-        var gato=this.hash
-        alert(gato);     
+        var gato=this.hash;
+        alert('prueba de bloque '+gato);     
         $('html, body').animate({scrollTop: $(gato).offset().top}, 800, function(){
         window.location.hash=gato; 
-        alert(gato)
+        alert('prueba de bloque '+gato);
             });
         } // Fin del if    
     })
